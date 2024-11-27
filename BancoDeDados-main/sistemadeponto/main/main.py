936b9da6ff7ffa17e67cdd86d28eb2764c1921c3
ProjetoBancoDeDados <sistemadebancodedadoss@gmail.com>
2024-11-27 14:36:08 -0300

    main.py

--- BancoDeDados-main/sistemadeponto/main/main.py
+++ BancoDeDados-main/sistemadeponto/main/main.py
@@ -10,5 +10,8 @@ from controllers.controller import SistemaController
 if __name__ == "__main__":
     with DatabaseConnection() as db_connection:
         print('Iniciando o sistema de controle de ponto,conectando ao banco de dados')
+        print("Sistema de Controle de Ponto")
+        print("Desenvolvido por: Julia Negri, Maria Isabel, Rafael Caires")
+        print("Professor: Howard Roatti")
         sistema = SistemaController(db_connection)
         sistema.exibir_menu_principal()
